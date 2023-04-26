bd03a92108c5dda9b9e8b436f7be69037cc78930
ClementEoche <clement.eoche35@gmail.com>
2023-04-26 14:48:16 +0200

    Fix : Fetching property

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,10 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = "http://localhost:8000";
 
-export async function apiRequest(endpoint, method = 'GET', data = null) {
+export async function apiRequest(endpoint, method, data) {
   const url = API_BASE_URL + endpoint;
   const options = {
     method,
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
+    credentials: 'include',
   };
 
   if (data) {
@@ -20,37 +18,38 @@ export async function apiRequest(endpoint, method = 'GET', data = null) {
   try {
     const response = await fetch(url, options);
     const jsonResponse = await response.json();
+    console.log("Response from", endpoint, " ", jsonResponse);
     return jsonResponse;
   } catch (error) {
-    console.error('Error fetching data from API:', error);
+    console.error("Error fetching data from", endpoint, " ", error);
     return null;
   }
 }
 
 export function getUsers() {
-  return apiRequest('/users');
+  return apiRequest("/users", "GET");
 }
 
 export function getRooms() {
-  return apiRequest('/rooms');
+  return apiRequest("/rooms", "GET");
 }
 
 export function getMessagesByRoomId(room_id) {
-  return apiRequest('/messages?room_id=' + room_id);
+  return apiRequest("/messages?room_id=" + room_id, "GET");
 }
 
 export function createUser(username) {
-  return apiRequest('/createuser', 'POST', { username });
+  return apiRequest("/createuser", "POST", { username });
 }
 
 export function createRoom(room_name) {
-  return apiRequest('/createroom', 'POST', { room_name });
+  return apiRequest("/createroom", "POST", { room_name });
 }
 
 export function postMessage(user_id, room_id, content) {
-  return apiRequest('/post-message', 'POST', { user_id, room_id, content });
+  return apiRequest("/post-message", "POST", { user_id, room_id, content });
 }
-
+/*
 export function disconnect() {
-  return apiRequest('/disconnect');
-}
+  return apiRequest("/disconnect");
+}*/
--- src/main.js
+++ src/main.js
@@ -1,29 +1,37 @@
-import Chat from './components/chat.js';
-import * as api from './api/index.js';
-import { isValidUsername, isValidRoomName, isValidMessageContent } from './utils/utils.js';
+import Chat from "./components/chat.js";
+import * as api from "./api/index.js";
+import {
+  isValidUsername,
+  isValidRoomName,
+  isValidMessageContent,
+} from "./utils/utils.js";
 
 (async function main() {
   const chat = new Chat();
 
   // Authentification de l'utilisateur et création d'un utilisateur
-  const username = prompt('Entrez votre nom d\'utilisateur:');
+  const username = prompt("Entrez votre nom d'utilisateur:");
   if (isValidUsername(username)) {
     const user = chat.createUser(username);
-    await api.createUser(username);
-    addUserToUserList(user);
+    const response = await api.createUser(username);
+    if (response) {
+      addUserToUserList(user);
+    }
   } else {
-    console.error('Nom d\'utilisateur invalide');
+    console.error("Nom d'utilisateur invalide");
     return;
   }
 
   // Création d'un salon
-  const roomName = prompt('Entrez le nom du salon à créer:');
+  const roomName = prompt("Entrez le nom du salon à créer:");
   if (isValidRoomName(roomName)) {
     const room = chat.createRoom(roomName);
-    await api.createRoom(roomName);
-    addRoomToRoomList(room);
+    const response = await api.createRoom(roomName);
+    if (response) {
+      addRoomToRoomList(room);
+    }
   } else {
-    console.error('Nom de salon invalide');
+    console.error("Nom de salon invalide");
     return;
   }
 
@@ -31,30 +39,30 @@ import { isValidUsername, isValidRoomName, isValidMessageContent } from './utils
   const rooms = await api.getRooms();
   const users = await api.getUsers();
 
-  rooms.forEach(room => addRoomToRoomList(room));
-  users.forEach(user => addUserToUserList(user));
+  rooms.forEach((room) => addRoomToRoomList(room));
+  users.forEach((user) => addUserToUserList(user));
 
   // Envoi et récupération des messages
   const userId = 0;
   const roomId = 0;
-  const sendButton = document.getElementById('send-button');
-  const messageInput = document.getElementById('message-input');
+  const sendButton = document.getElementById("send-button");
+  const messageInput = document.getElementById("message-input");
 
-  sendButton.addEventListener('click', async () => {
+  sendButton.addEventListener("click", async () => {
     const content = messageInput.value;
     if (isValidMessageContent(content)) {
       const message = chat.postMessage(userId, roomId, content);
       await api.postMessage(userId, roomId, content);
       addMessageToMessages(message);
-      messageInput.value = '';
+      messageInput.value = "";
     } else {
-      console.error('Contenu de message invalide');
+      console.error("Contenu de message invalide");
       return;
     }
   });
 
   const messages = await api.getMessagesByRoomId(roomId);
-  messages.forEach(message => addMessageToMessages(message));
+  messages.forEach((message) => addMessageToMessages(message));
 
   // Déconnexion de l'utilisateur
   //await api.disconnect();
@@ -62,22 +70,22 @@ import { isValidUsername, isValidRoomName, isValidMessageContent } from './utils
 })();
 
 function addRoomToRoomList(room) {
-  const roomList = document.querySelector('.room-list');
-  const roomItem = document.createElement('li');
+  const roomList = document.querySelector(".room-list");
+  const roomItem = document.createElement("li");
   roomItem.textContent = room.name;
   roomList.appendChild(roomItem);
 }
 
 function addUserToUserList(user) {
-  const userList = document.querySelector('.user-list');
-  const userItem = document.createElement('li');
+  const userList = document.querySelector(".user-list");
+  const userItem = document.createElement("li");
   userItem.textContent = user.username;
   userList.appendChild(userItem);
 }
 
 function addMessageToMessages(message) {
-  const messagesDiv = document.getElementById('messages');
-  const messageItem = document.createElement('div');
+  const messagesDiv = document.getElementById("messages");
+  const messageItem = document.createElement("div");
   messageItem.textContent = `${message.user_id}: ${message.content}`;
   messagesDiv.appendChild(messageItem);
 }
